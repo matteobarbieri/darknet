4b5ecd5d670a252614e63873ddf22f00bb898f6b
Matteo Barbieri <neopard@gmail.com>
2019-09-10 17:29:24 +0200

    Update print to Python 3 and make lib path relative

--- python/darknet.py
+++ python/darknet.py
@@ -1,6 +1,7 @@
 from ctypes import *
-import math
 import random
+
+import os
 
 def sample(probs):
     s = sum(probs)
@@ -42,10 +43,17 @@ class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
 
-    
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+# lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+# lib = CDLL("darknet/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "libdarknet.so"),
+    RTLD_GLOBAL)
+
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -124,6 +132,7 @@ def classify(net, meta, im):
 
 def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
     im = load_image(image, 0, 0)
+    wh = (im.w,im.h)
     num = c_int(0)
     pnum = pointer(num)
     predict_image(net, im)
@@ -140,9 +149,10 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
     res = sorted(res, key=lambda x: -x[1])
     free_image(im)
     free_detections(dets, num)
-    return res
-    
+    return res, wh
+
 if __name__ == "__main__":
+
     #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
     #im = load_image("data/wolf.jpg", 0, 0)
     #meta = load_meta("cfg/imagenet1k.data")
@@ -151,6 +161,6 @@ if __name__ == "__main__":
     net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
     meta = load_meta("cfg/coco.data")
     r = detect(net, meta, "data/dog.jpg")
-    print r
-    
+    print(r)
+
 
